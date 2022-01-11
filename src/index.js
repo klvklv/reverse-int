3563f5dc707c9d9bfc86a3b0ce24d887a073b44d
klvklv <une_dina@mail.ru>
2022-01-11 22:13:51 +0200

    task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+   let num = Math.abs(n);
+	let str = num.toString();
+	let strLength = str.length;
+	let result = "";
+	for(let i = strLength - 1; i >= 0; i--){
+		result = result + str[i];
+	}
+	let n1 = parseInt(result);
+	return n1;
 }
